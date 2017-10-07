c96748210ed64fc1e40a66c1210228e93c94b9f6
alamin81 <32478452+alamin81@users.noreply.github.com>
2017-10-07 12:24:02 +0600

    class-two

--- class-two/js/active.js
+++ class-two/js/active.js
@@ -1,16 +1,42 @@
-$(function(){
+$(document).ready(function(){
+    
+	$('.active-slider').owlCarousel({
+    loop:true,
+	items:1,
+	smartSpeed:1500,
+    nav:true,
+	navText:['<i class="fa fa-angle-left">','<i class="fa fa-angle-right">'],
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})    
+	$('.testimonial-active').owlCarousel({
+    loop:true,
+	items:1,
+	smartSpeed:1500,
+    nav:true,
+	animateOut: 'slideOutUp',
+    animateIn: 'flipInX',
+    navText:['<i class="fa fa-angle-left"></i>','<i class="fa fa-angle-right"></i>'],
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
 
-	// Instantiate MixItUp:
-
-	$('#Container').mixItUp();
-	
-	$('.image-link').magnificPopup({
-    type: 'image',
-    gallery:{
-    enabled:true
-  }
-});
-});
-
-
-	
+}); 
